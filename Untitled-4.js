21a874eead961ebc1db93a327e126b28432a4bd7
IQRA-WAHEED1 <iqrawaheed1324@gmail.com>
2024-07-31 17:54:30 +0500

    Add files via upload

---
+++ Untitled-4.js
@@ -0,0 +1,10 @@
+// server.js (updated)
+io.on('connection', (socket) => {
+  console.log('a user connected');
+  socket.on('chat message', (msg) => {
+    io.emit('chat message', msg);
+  });
+  socket.on('disconnect', () => {
+    console.log('user disconnected');
+  });
+});
